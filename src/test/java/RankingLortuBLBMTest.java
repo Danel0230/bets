72e99dcd7e8ef0d4ab588d22f9f0506bbdeca744
Iñigo Gil <inigogil@bilbokoeskolapioak.org>
2023-10-15 20:09:41 +0200

    RankingLortuBLMTest.java

--- src/test/java/RankingLortuBLBMTest.java
+++ src/test/java/RankingLortuBLBMTest.java
@@ -1,4 +1,49 @@
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import businessLogic.BLFacade;
+import businessLogic.BLFacadeImplementation;
+import dataAccess.DataAccess;
+import domain.Registered;
+
+@RunWith(MockitoJUnitRunner.class)
 public class RankingLortuBLBMTest {
 
+	@Mock
+     DataAccess dataAccess;
+
+	@InjectMocks
+	 BLFacade sut=new BLFacadeImplementation(dataAccess);
+	
+	@Test
+	public void test1() {
+		try {
+			//Ranking normala itzultzen du:
+			ArrayList<Registered> expectedEma = new ArrayList<Registered>();
+			expectedEma.add(new Registered("arantxa", "1234", 1111));
+			expectedEma.add(new Registered("markel", "pass123", 1111));
+			expectedEma.add(new Registered("andoni", "qwerty", 1111));
+			
+			ArrayList<Registered> expectedMocked = new ArrayList<Registered>();
+			expectedMocked.add(new Registered("arantxa", "1234", 1111));
+			expectedMocked.add(new Registered("markel", "pass123", 1111));
+			expectedMocked.add(new Registered("andoni", "qwerty", 1111));
+			
+			Mockito.doReturn(expectedMocked).when(sut).rankingLortu();
+			
+			assertEquals(expectedEma, sut.rankingLortu());
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
